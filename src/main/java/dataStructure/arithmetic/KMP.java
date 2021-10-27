8f58557ddc7bc1cdf960c4a05262297dbb0815b9
kewudenao <422826233@qq.com>
2021-10-27 10:53:06 +0800

    kmp+next

--- src/main/java/dataStructure/arithmetic/KMP.java
+++ src/main/java/dataStructure/arithmetic/KMP.java
@@ -2,9 +2,11 @@ package dataStructure.arithmetic;
 // kmp  https://blog.csdn.net/v_july_v/article/details/7041827#t10
 public class KMP {
 	public static void main(String[] args) {
-
+		String s1 = "aeeadefqewabc";
+		String s2 = "abc";
 		int[] next = GetNext("abcdef");
-		System.out.println(next);
+		int index = kmpSearch(s1,s2,next);
+		System.out.println(index);
 	}
 
 	static int[] GetNext(String p1) {
@@ -25,6 +27,25 @@ public class KMP {
 			}
 		}
 		return next;
+	}
+
+	static int kmpSearch(String s1,String s2,int[] next){
+		int i=0;
+		int j=0;
+		int sLen = s1.length();
+		int pLen = s2.length();
+		while (i<sLen&&j <pLen){
+			if (j==-1||s1.charAt(i)==s2.charAt(j)){
+				i++;
+				j++;
+			}else {
+				j = next[j];
+			}
+		}
+		if(j == pLen)
+			return i - j;
+		else
+			return  -1;
 	}
 }
 
